2f7898c7b8a42920dc04ad115580a3ce2d546740
IrvinPoncePerez <IrvinPoncePerez@gmail.com>
2015-07-18 20:28:07 -0500

    removing menu settings

--- app/src/main/java/com/example/irvin/integrate_zxing/HomeActivity.java
+++ app/src/main/java/com/example/irvin/integrate_zxing/HomeActivity.java
@@ -35,10 +35,10 @@ public class HomeActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+          //  return true;
+        //}
 
-        return super.onOptionsItemSelected(item);
+        return false;//super.onOptionsItemSelected(item);
     }
 }
