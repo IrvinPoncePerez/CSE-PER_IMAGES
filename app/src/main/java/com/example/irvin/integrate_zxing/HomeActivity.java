2065168799afc4fa8242fab20005729d71210e59
IrvinPoncePerez <IrvinPoncePerez@gmail.com>
2015-07-19 17:32:42 -0500

    reading and showing result of scanner, using AlertDialog.

--- app/src/main/java/com/example/irvin/integrate_zxing/HomeActivity.java
+++ app/src/main/java/com/example/irvin/integrate_zxing/HomeActivity.java
@@ -1,12 +1,22 @@
 package com.example.irvin.integrate_zxing;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.view.View;
+import android.content.Intent;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import android.content.Context;
+
 
 public class HomeActivity extends ActionBarActivity {
+
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,4 +51,47 @@ public class HomeActivity extends ActionBarActivity {
 
         return false;//super.onOptionsItemSelected(item);
     }
+
+    public void btnScan_onClick(View view){
+        IntentIntegrator integrator = new IntentIntegrator(HomeActivity.this);
+        integrator.addExtra("SCAN_WIDTH", 640);
+        integrator.addExtra("SCAN_HEIGHT", 480);
+        //tegrator.addExtra("SCAN_MODE", "QR_CODE_MODE,PRODUCT_MODE,");
+        integrator.addExtra("SCAN_FORMATS", "CODE_39");
+
+        integrator.addExtra("PROMPT_MESSAGE", "Scanner Start!");
+        integrator.initiateScan(IntentIntegrator.ALL_CODE_TYPES);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setTitle(R.string.app_name);
+
+        if (result != null) {
+            String contents = result.getContents();
+            if (contents != null) {
+                alertDialogBuilder.setMessage(result.toString());
+            } else {
+                alertDialogBuilder.setMessage("Don't result scanner");
+            }
+
+            alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int id){
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+
+        }
+
+    }
+
+
 }
