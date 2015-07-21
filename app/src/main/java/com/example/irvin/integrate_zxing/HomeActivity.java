a82f336e064341a16ce9f014fe5c53a467cf637c
IrvinPoncePerez <IrvinPoncePerez@gmail.com>
2015-07-20 21:42:46 -0500

    modifying version2

--- app/src/main/java/com/example/irvin/integrate_zxing/HomeActivity.java
+++ app/src/main/java/com/example/irvin/integrate_zxing/HomeActivity.java
@@ -46,11 +46,11 @@ public class HomeActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        //if (id == R.id.action_settings) {
-          //  return true;
-        //}
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-        return false;//super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);
     }
 
     public void btnScan_onClick(View view){
