3610207ff578bc42d45dc2256122cb326e573ae0
Irvin Ponce Perez <IrvinPoncePerez@gmail.com>
2015-07-23 17:50:59 -0500

    connecting HTTPURL.

--- app/src/main/java/com/example/irvin/integrate_zxing/SetSettingsActivity.java
+++ app/src/main/java/com/example/irvin/integrate_zxing/SetSettingsActivity.java
@@ -1,7 +1,6 @@
 package com.example.irvin.integrate_zxing;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
@@ -11,7 +10,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.irvin.integrate_zxing.DatabaseHandler.SETTING;
 
